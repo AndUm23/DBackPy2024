c81e697d9ba1f71983b3e6046cb786c666b8af88
AndUm23 <edg-andres@hotmail.com>
2024-02-27 00:48:44 -0500

    agregadas funvionalidades

--- calculadora.py
+++ calculadora.py
@@ -4,5 +4,8 @@ def suma (a, b):
 def resta(a,b):
     return a-b 
 
-def division (a,b):
+def division(a,b):
     return a/b
+
+def multiplicacion(a,b):
+    return a*b
--- main.py
+++ main.py
@@ -2,18 +2,37 @@ from calculadora import *
 
 option = input("""
 Seleccione una operación
-1. Suma 
-2. Resta
-3. Division
+1: Suma 
+2: Resta
+3: Division
+4: Multiplicación
 """)
 
 try:
     option = int(option)
+    
+    if option not in [1,2,3,4]:
+        exit()
+    
+    a = int(input("Digite el primer numero: "))
+    b = int(input("Digite el segundo numero: "))
+    
+    if option == 1:
+        print("Suma: " + str(suma(a,b)))
+        
+    elif option == 2:
+        print("Resta: " + str(resta()))
+        
+    elif option == 3:
+        print("Division: " + str(division(a,b)))
+        
+    elif option == 4:
+        print("Multiplicación: " + str(multiplicacion(a,b)))
+              
+    else:
+        print ("Valor no válido")
 except:
-    print ("valor no valido")
-
-if option not in [1,2,3]:
-    print ("Valor no valido")
-    exit()
+    print ("valor no válido")
+    
 
 
